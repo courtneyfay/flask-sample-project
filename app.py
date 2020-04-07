834e84b3b3e66fba2aa1a8f5de08c934d5ce296c
Courtney Fay <cfay@nexthealthtechnologies.com>
2020-04-07 16:03:40 -0400

    adding templates and bootstrap to project

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    fruits = ['Apple', 'Orange']
+    return render_template('index.html', fruits=fruits)
+    # return redirect(url_for('about'))
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/css')
+def css():
+    return render_template('css.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=3000)
