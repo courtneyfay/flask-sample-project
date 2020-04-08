b3ab280b75e01aab4d49c1f6cd98dba5496c8eae
Courtney Fay <cfay@nexthealthtechnologies.com>
2020-04-08 08:57:03 -0400

    adding connection to local mysql database'

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template, url_for, redirect
+from flask_mysqldb import MySQL
 from flask_bootstrap import Bootstrap
+import yaml
 
 app = Flask(__name__)
 Bootstrap(app)
 
+# Configure DB
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
+
 @app.route('/')
 def index():
-    fruits = ['Apple', 'Orange']
-    return render_template('index.html', fruits=fruits)
+    cursor = mysql.connection.cursor()
+    # cursor.execute('INSERT INTO user VALUES(%s)', ['Mike'])
+    # mysql.connection.commit()
+    result_value = cursor.execute('SELECT * FROM user')
+    if result_value > 0:
+        users = cursor.fetchall()
+        # print(users)
+    # fruits = ['Apple', 'Orange']
+    return render_template('index.html')
+    # , fruits=fruits)
     # return redirect(url_for('about'))
 
 @app.route('/about')
